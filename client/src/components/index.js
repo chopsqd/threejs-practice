b59e265801bfb280be60d60e711ac67fb3b8943f
Michael Chop <007micle007@gmail.com>
2023-11-24 23:15:22 +0300

    Finished work with some components

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton.jsx";
+import AIPicker from "./AIPicker.jsx";
+import ColorPicker from "./ColorPicker.jsx";
+import FilePicker from "./FilePicker.jsx";
+import Tab from "./Tab.jsx";
+
+export {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab
+}
